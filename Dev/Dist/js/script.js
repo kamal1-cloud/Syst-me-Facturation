b4a441b508a86d865774ba997d7d9e4d961ad907
Majda FANNAN <fannanmajda123@gmail.com>
2020-06-21 22:40:09 +0100

    commit

--- Dev/Dist/js/script.js
+++ Dev/Dist/js/script.js
@@ -12,37 +12,10 @@ addbuttom.addEventListener("click", function () {
     counter ++
     console.log(counter)
     var x = document.createElement("form");
-    x.innerHTML = " <div id='form1' class='d-flex flex-row justify-content-around align-items-center'><div class='form-group'><label for='nomproduit'> Le nom de produits</label><input type='text' id='nomproduit' class='form-control'></div><div class='form-group'><label for='quantite'>La quantité</label><input type='text' id='quantite' class='form-control'></div><div class='form-group'><label for='price'>Prix unitaire</label><input type='text' id='price' class='form-control'></div><div class='form-group'><label for='tva'>TVA</label><select name='tva' id='tva' name='tva' class='form-control'><option value=''>--Please choose an option--</option><option value='7'>7%</option><option value='10'>10%</option><option value='14'>14%</option><option value='20'>20%</option></select></div><div class='form-group'><label for='R1'>Total</label><p type='text' id='R1' class='form-control px-5'>00.00</p></div> <div onclick='del(this)'><img src='delete.svg' alt='' width='20px'></div>";
+    x.innerHTML = " <div id='form1' class='d-flex flex-row justify-content-around align-items-center'><div class='form-group'><label for='nomproduit'> Le nom de produits</label><input type='text' id='nomproduit' class='form-control'></div><div class='form-group'><label for='quantite'>La quantité</label><input type='text' id='quantite' class='form-control'></div><div class='form-group'><label for='price'>Prix unitaire</label><input type='text' id='price' class='form-control'></div><div class='form-group'><label for='tva'>TVA</label><select name='tva' id='tva' name='tva' class='form-control'><option value=''>--Please choose an option--</option><option value='7'>7%</option><option value='10'>10%</option><option value='14'>14%</option><option value='20'>20%</option></select></div><div class='form-group'><label for='R1'>Total</label><p type='text' id='R1' class='form-control px-5'>00.00</p></div> <div onclick='del(this)'><img src='Design/delete.svg' alt='' width='20px'></div>";
     container.appendChild(x)
 
 
-
-    // counter++
-    // if (counter == limit) {
-    //     alert('you can't add more row then " + counter + " form")
-
-    // } else {
-
-    //     switch (counter) {
-    //         case (2):
-    //             var form2 = document.getElementById("form2")
-    //             form2.classList.remove("d-none");
-
-
-    //             break;
-    //         case (3):
-    //             var form3 = document.getElementById("form3")
-    //             form3.classList.remove("d-none");
-
-    //             break;
-    //         case (4):
-    //             var form4 = document.getElementById("form4")
-    //             form4.classList.remove("d-none");
-
-    //             break;
-
-    //     }
-    // }
 });
 
 
